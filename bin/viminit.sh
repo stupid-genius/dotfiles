1f9ad723b31e08f0b47614f366d5e3cdf5700214
Allen Ng <allen.ng@anark.com>
2021-08-08 17:12:46 -0600

    Switched to ALE
    Now I don't have to install each language server

--- bin/viminit.sh
+++ bin/viminit.sh
@@ -9,9 +9,11 @@ git clone https://tpope.io/vim/surround.git
 vim -u NONE -c "helptags surround/doc" -c q
 git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki
 vim -c 'helptags ~/.vim/pack/plugins/start/vimwiki/doc' -c quit
-mkdir -p ~/.vim/pack/vim-lsc/start
-git clone --depth=1 https://github.com/natebosch/vim-lsc.git ~/.vim/pack/vim-lsc/start/vim-lsc
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+#mkdir -p ~/.vim/pack/vim-lsc/start
+#git clone --depth=1 https://github.com/natebosch/vim-lsc.git ~/.vim/pack/vim-lsc/start/vim-lsc
 
-mkdir -p ~/.vim/bin; cd $_
-npm i bash-language-server
+#mkdir -p ~/.vim/bin; cd $_
+#npm i bash-language-server
 #git clone https://github.com/stupid-genius/javascript-typescript-langserver.git
